9fd73dee872566b62ca2367a9e5a7281e02265ca
jtyaaj <jtyaaj01@gmail.com>
2026-01-29 21:12:52 -0600

    Calculation and Order summary methods

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,49 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    //Calculation Joshua Yang
+    public float calculateTotalPrice(Order o){
+
+        Order current = o;
+        float total = 0;
+
+        while(current != null){
+            //multiplies by the quantity and the price and adds it to total
+            total +=  current.getprice() * current.getquantity();
+            //traverses the whole list
+            current = current.next;
+        }
+
+        return total;
+    }
+
+    public String getUncompletedOrdersSummary(Order allOrders){
+
+        Order current = allorders;
+        StringBuilder sb = new StringBuilder();
+        float total = 0;
+
+        while(current != null){
+            sb.append(current.data); //name of the item
+            sb.append("\n");
+            sb.append(current.getquantity());
+            sb.append("\n");
+            sb.append(current.getprice());
+            sb.append("\n");
+            total +=  current.getprice() * current.getquantity();
+            current = current.next;
+        }
+
+        sb.append("Total will be: " + total);
+
+        String result  = sb.toString();
+
+        return result;
+
+    }
+
+
+
 }
